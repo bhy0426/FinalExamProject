18f639b370531639fa6e3d9051c41c59d5680b28
Hayeon Byun <bhy246@naver.com>
2022-12-11 17:54:48 +0900

    2022_12_11
    완성?

--- header.h
+++ header.h
@@ -4,27 +4,17 @@
 #define MAX_COL 3
 
 // 구조체 선언
-struct Menu {
+typedef struct _Menu {
 	int num;
 	char *name;
 	int cost;
-};
-struct selectedMenu {
-	struct Menu menu;
+}Menu;
+typedef struct _selectedMenu {
+	Menu *menu;
 	short count;
-};
+}selectedMenu;
 
-// 전역 변수 정의
-// - 구조체
-struct Menu menuList[MAX_ROW][MAX_COL] =
-{ {{10, "싸이버거세트", 6600}, {11, "화이트갈릭버거세트", 6900}, {12, "휠렛버거세트", 6400}},
-{{20, "싸이버거", 4300}, {21, "화이트갈릭버거", 4600}, {22, "휠렛버거", 4100}},
-{{30, "케이준 양념 감자(소)", 1800}, {31, "케이준 양념 감자(중)", 3200}, {32, "치즈스틱", 2000}},
-{{40, "콜라", 1600}, {41, "오렌지주스", 2000}, {42, "청포도에이드", 2200}} };
-struct selectedMenu cart[MAX_LIST];
-struct selectedMenu menuLog[MAX_ROW][MAX_COL];
-FILE* fp;
-
-// - 정수형
+// 전역
+// - int
 int listTop = 0;
 int result;
--- main_noFile.c
+++ main_noFile.c
@@ -1,7 +1,7 @@
 
 ///////////////////////////////////////////////////////////
 // 
-// 2022-11-26 Sat                                        
+// 2022-12-10 Sat                                        
 // <패스트푸트 전문점의 메뉴선택 및 금전등록기 처리 프로그램>
 // '맘스터치 키오스크'
 // 
@@ -12,24 +12,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <conio.h>
 // 매크로 상수 정의, 구조체 정의, 전역변수 정의가 들어있는 헤더파일 header.h
 #include "header.h"
 
 // 사용자 정의 함수 정의
-void printMenu();
-void add();
-void cancel();
-void quit();
-void initiate();
-void printCart();
-void recommend();
+void printMenu(const selectedMenu* cart);
+void add(selectedMenu* cart, const selectedMenu(*menuLog)[MAX_COL]);
+void cancel(selectedMenu* cart);
+void quit(FILE* fp, const selectedMenu *cart, selectedMenu (*menuLog)[MAX_COL]);
+void reset(selectedMenu* cart);
+void printCart(const selectedMenu* cart);
+void recommend(int maxNum, const selectedMenu(*menuLog)[MAX_COL]);
+void initiate(FILE* fp, int* maxNum, selectedMenu *cart,selectedMenu(*menuLog)[MAX_COL]);
 
 // 1. main()
 // - 메뉴를 보여주고 무슨 작업을 할지 결정하는 메인 화면
 int main(void)
 {
+	// 지역 변수 선언 & 초기화
+	FILE* fp;
+	int numMain = 0;
+	int maxNum = 0;
+	selectedMenu cart[MAX_LIST];
+	selectedMenu menuLog[MAX_ROW][MAX_COL];
+
+	// 초기화 작업
 	// menu_log.txt 파일을 열고
 	fp = fopen("menu_log.txt", "r+");
 	if (fp == NULL)
@@ -37,39 +45,28 @@ int main(void)
 		printf("파일 열기 오류");
 		exit(1);
 	}
-	// 데이터를 menuLog 구조체 배열에 저장
-	for (int r = 0; r < MAX_ROW; r++)
-	{
-		for (int c = 0; c < MAX_COL; c++)
-		{
-			char ch[12][20];
-			fscanf(fp, "%d %s %d %d", &menuLog[r][c].menu.num, ch[(3 * r) + c], &menuLog[r][c].menu.cost, &menuLog[r][c].count);
-			menuLog[r][c].menu.name = ch[(3 * r) + c];
-		}
-	}
-	// menuLog를 바탕으로 고객들이 가장 많이 고른 메뉴를 추천
-	recommend();
-	
-	int numMain;
+	// 파일에 있는 데이터를 menuLog에 가져오고 가장 높은 count를 maxNum에 저장
+	initiate(fp, &maxNum, cart, menuLog);
 
 	while (1)
-	{
-		printMenu();
-		
+	{	
+		// menuLog를 바탕으로 고객들이 가장 많이 고른 메뉴를 출력
+		recommend(maxNum, menuLog);
+		printMenu(cart, menuLog);
 		scanf("%d", &numMain);
 		switch (numMain)
 		{
-		case 0: // 총 금액과 cart 구조체 배열 초기화
-			initiate();
+		case 0: 
+			reset(cart); // 총 금액과 cart 구조체 배열 초기화
 			break;
 		case 1:
-			add(); // 메뉴 추가
+			add(cart, menuLog); // 메뉴 추가
 			break;
 		case 2:
-			cancel(); // 메뉴 취소
+			cancel(cart); // 메뉴 취소
 			break;
 		case 3:
-			quit(); // cart에 담긴 메뉴 계산 
+			quit(fp, cart, menuLog); // cart에 담긴 메뉴 계산 
 			break;
 		}
 		// 콘솔 화면 지우기
@@ -80,7 +77,7 @@ int main(void)
 
 // 2. printMenu()
 // - 메뉴를 출력
-void printMenu()
+void printMenu(const selectedMenu *cart, const selectedMenu(*menuLog)[MAX_COL])
 {
 
 	for (int r = 0; r < MAX_ROW; r += 2)
@@ -94,14 +91,11 @@ void printMenu()
 		else if (r == 2)
 			printf("<디저트메뉴>\t\t\t<음료메뉴>\n");
 		for (int c = 0; c < MAX_COL; c++)
-		{
-			struct Menu x = menuList[r][c];
-			struct Menu y = menuList[r + 1][c];
-			printf("%d %20s %5d\t%d %20s %5d\n", x.num, x.name, x.cost, y.num, y.name, y.cost);
-		}
+			printf("%d %20s %5d\t%d %20s %5d\n", menuLog[r][c].menu->num, menuLog[r][c].menu->name, menuLog[r][c].menu->cost, menuLog[r + 1][c].menu->num, menuLog[r + 1][c].menu->name, menuLog[r + 1][c].menu->cost);
+
 		printf("\n");
 	}
-	printCart();
+	printCart(cart); // cart에 담긴 메뉴 출력
 	printf("지불할 총액 :       %d\n\n", result);
 	printf("1. 추가\n2. 취소\n3. 입력종료\n0. 처음화면(초기화)\n");
 	printf("\n번호 선택 후 Enter > ");
@@ -110,7 +104,7 @@ void printMenu()
 // 3. add()
 // - 추가할 메뉴를 고르고 수량을 입력
 // 입력된 내용을 cart 구조체 배열에 저장
-void add()
+void add(selectedMenu *cart, const selectedMenu (*menuLog)[MAX_COL])
 {
 	int numMenu, numCount, num;
 	int isCorrect = 0;
@@ -131,7 +125,7 @@ void add()
 		{
 			for (int c = 0; c < MAX_COL; c++)
 			{
-				if (numMenu == menuList[r][c].num)
+				if (numMenu == menuLog[r][c].menu->num)
 					isCorrect = 1;
 			}
 		}
@@ -144,7 +138,7 @@ void add()
 	// cart에 저장된 구조체를 찾아 count만 변경해줌
 	for (int i = 0; i < listTop; i++)
 	{
-		if (numMenu == cart[i].menu.num)
+		if (numMenu == cart[i].menu->num)
 		{
 			while (1)
 			{
@@ -153,16 +147,16 @@ void add()
 
 				if (num == 1)
 				{
-					result -= cart[i].menu.cost * cart[i].count;
+					result -= cart[i].menu->cost * cart[i].count;
 					printf("수량 입력 후 Enter > ");
 					scanf("%d", &numCount);
 					cart[i].count = numCount;
-					result += menuList[(numMenu / 10) - 1][numMenu % 10].cost * numCount;
+					result += menuLog[(numMenu / 10) - 1][numMenu % 10].menu->cost * numCount;
 					return;
 				}
 				if (num == 2)
 				{
-					add();
+					add(cart, menuLog);
 					return;
 				}
 				else
@@ -181,16 +175,16 @@ void add()
 	{
 		for (int c = 0; c < MAX_COL; c++)
 		{
-			if (numMenu == menuList[r][c].num)
+			if (numMenu == menuLog[r][c].menu->num)
 			{
-				cart[listTop].menu = menuList[r][c];
+				cart[listTop].menu = menuLog[r][c].menu;
 				cart[listTop].count = numCount;
 				listTop++;
 			}
 		}
 	}
 	// result에 고른 메뉴의 가격과 수량을 곱하여 대입
-	result += menuList[(numMenu / 10) - 1][numMenu % 10].cost * numCount;
+	result += menuLog[(numMenu / 10) - 1][numMenu % 10].menu->cost * numCount;
 	printf("result = %d\n", result);
 }
 
@@ -198,13 +192,13 @@ void add()
 // - 선택된 메뉴들 중 취소할 메뉴를 고름
 // cart에 저장된 메뉴들 중 특정 메뉴를 지우고 싶으면
 // 배열의 특정 요소를 입력받은 후 그 다음 요소를 한 칸씩 왼쪽으로 당김, listTop도 1감소
-void cancel()
+void cancel(selectedMenu* cart)
 {
 	int num;
 	int isFound = 0;
 
 	printf("\n");
-	printCart();
+	printCart(cart);
 	while (1)
 	{
 		printf("취소할 메뉴버튼 입력 후 Enter(0 : cancel) > ");
@@ -213,10 +207,10 @@ void cancel()
 			return;
 		for (int i = 0; i < listTop; i++)
 		{
-			if (num == cart[i].menu.num)
+			if (num == cart[i].menu->num)
 			{
 				isFound = 1;
-				result -= cart[i].menu.cost * cart[i].count;
+				result -= cart[i].menu->cost * cart[i].count;
 			}
 			if (isFound == 1)
 				cart[i] = cart[i + 1];
@@ -233,7 +227,7 @@ void cancel()
 
 // 5. quit()
 // - 고객에게 받은 금액을 입력 후 계산
-void quit()
+void quit(FILE *fp, const selectedMenu *cart, selectedMenu(*menuLog)[MAX_COL])
 {
 	int num = 0;
 	int bill;
@@ -266,7 +260,7 @@ void quit()
 		else break;
 	}
 	printf("\n명세표\n");
-	printCart();
+	printCart(cart);
 	printf("금액 : %d\n", bill);
 	printf("잔돈 : %d\n", bill - result);
 	fseek(fp, 0L, SEEK_SET);
@@ -276,7 +270,7 @@ void quit()
 		{
 			for (int c = 0; c < MAX_COL; c++)
 			{
-				if (cart[i].menu.num == menuLog[r][c].menu.num)
+				if (cart[i].menu->num == menuLog[r][c].menu->num)
 					menuLog[r][c].count += cart[i].count;
 			}
 		}
@@ -286,51 +280,46 @@ void quit()
 		for (int c = 0; c < MAX_COL; c++)
 		{
 		
-			fprintf(fp, "%d %s %d %d\n", menuLog[r][c].menu.num, menuLog[r][c].menu.name, menuLog[r][c].menu.cost, menuLog[r][c].count);
-			printf("%d %s %d %d\n", menuLog[r][c].menu.num, menuLog[r][c].menu.name, menuLog[r][c].menu.cost, menuLog[r][c].count);
+			fprintf(fp, "%d %s %d %d\n", menuLog[r][c].menu->num, menuLog[r][c].menu->name, menuLog[r][c].menu->cost, menuLog[r][c].count);
+			printf("%d %s %d %d\n", menuLog[r][c].menu->num, menuLog[r][c].menu->name, menuLog[r][c].menu->cost, menuLog[r][c].count);
+			//free(menuLog[r][c].menu);
+			//free(cart[(3 * r) + c].menu);
 		}
 	}
 	fclose(fp);
 	exit(1);
 }
 
-// 6. initiate()
+// 6. reset()
 // - 오류가 발생했거나 고객이 금액이 부족하여 계산을 하지 못한 경우 다시 초기화
-void initiate()
+void reset(selectedMenu *cart)
 {
 	result = 0;
-	struct selectedMenu x = { {0, "0", 0}, 0 };
-	for (int i = 0; i < MAX_LIST; i++)
-		cart[i] = x;
+	listTop = 0;
+	for (int i = 0; i < MAX_LIST-1; i++)
+	{
+		cart[i].menu = NULL;
+		cart[i].count = cart[MAX_LIST].count;
+	}
 }
 
 // 7. printCart()
 // - 선택된 메뉴를 출력
-void printCart()
+void printCart(const selectedMenu *cart)
 {
 	printf("<선택된 메뉴>\n");
 	for (int i = 0; i < listTop; i++)
 	{
-		struct selectedMenu x = cart[i];
-		printf("%d %s %d x %d = %d\n", x.menu.num, x.menu.name, x.menu.cost, x.count, x.menu.cost * x.count);
+		printf("%d %s %d x %d = %d\n", cart[i].menu->num, cart[i].menu->name, cart[i].menu->cost, cart[i].count, (cart[i].menu->cost)*(cart[i].count));
 	}
 	printf("\n");
 }
 
-
 // 8. recommend()
 // - 파일에서 읽어온 데이터를 바탕으로 사용자에게 메뉴를 추천
-void recommend()
+void recommend(int maxNum, const selectedMenu(*menuLog)[MAX_COL])
 {
-	int maxNum = 0;
-	for (int r = 0; r < MAX_ROW; r++)
-	{
-		for (int c = 0; c < MAX_COL; c++)
-		{
-			if (maxNum <= menuLog[r][c].count)
-				maxNum = menuLog[r][c].count;
-		}
-	}
+
 	printf("<*추천메뉴*>\n");
 	for (int r = 0; r < MAX_ROW; r++)
 	{
@@ -338,9 +327,36 @@ void recommend()
 		{
 			if (menuLog[r][c].count == maxNum)
 			{
-				printf("%d %20s %5d\n", menuList[r][c].num, menuList[r][c].name, menuList[r][c].cost);
+				printf("%d %20s %5d\n", menuLog[r][c].menu->num, menuLog[r][c].menu->name, menuLog[r][c].menu->cost);
 			}
 		}
 	}
 	printf("\n");
+}
+
+// 9. initiate()
+// - 파일에 있는 데이터를 menuLog에 가져오고 가장 높은 count를 maxNum에 저장
+void initiate(FILE* fp , int *maxNum, selectedMenu *cart, selectedMenu (*menuLog)[MAX_COL]) 
+{
+	// 데이터를 menuLog 구조체 배열에 저장
+	for (int r = 0; r < MAX_ROW; r++)
+	{
+		for (int c = 0; c < MAX_COL; c++)
+		{
+			menuLog[r][c].menu = (Menu*)malloc(sizeof(Menu));
+			cart[(3 * r) + c].menu = (Menu*)malloc(sizeof(Menu));
+			menuLog[r][c].menu->name = (char*)malloc(sizeof(char) * 20);
+			fscanf(fp, "%d %s %d %hd", &menuLog[r][c].menu->num, menuLog[r][c].menu->name, &menuLog[r][c].menu->cost, &menuLog[r][c].count);
+			//printf("%d %s %d %d\n", menuLog[r][c].menu->num, menuLog[r][c].menu->name, menuLog[r][c].menu->cost, menuLog[r][c].count); // 디버깅용
+		}
+	}
+	// menuLog에서 가장 높은 count를 maxNum에 저장
+	for (int r = 0; r < MAX_ROW; r++)
+	{
+		for (int c = 0; c < MAX_COL; c++)
+		{
+			if (*maxNum <= menuLog[r][c].count)
+				*maxNum = menuLog[r][c].count;
+		}
+	}
 }
